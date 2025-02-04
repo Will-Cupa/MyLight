3a4a8e21d37aeb4297acfe14c1ec1723e47f9146
Alain Sandoz <alain.sandoz@universite-paris-saclay.fr>
2025-02-04 10:32:51 +0100

    finally some code

--- app/src/main/java/com/example/mylight/MainActivity.java
+++ app/src/main/java/com/example/mylight/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mylight;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button[] buttonList;
+    private Button preview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        buttonList = new Button[6];
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //Toolbar padding
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ViewGroup main = (ViewGroup) findViewById(R.id.main);
+
+        for(int i=0; i < main.getChildCount(); i++){
+            View child = main.getChildAt(i);
+            if (child instanceof Button){
+                buttonList[i] = (Button) child;
+            }
+        }
+
+        buttonList[0] = findViewById(R.id.addRed);
+        buttonList[1] = findViewById(R.id.remRed);
+
+
     }
 }
