740a4ec9e864dfa8061284cf7f078e1f0cf20640
Alain Sandoz <alain.sandoz@universite-paris-saclay.fr>
2025-02-11 09:56:51 +0100

    lamp color

--- app/src/main/java/com/example/mylight/MainActivity.java
+++ app/src/main/java/com/example/mylight/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mylight;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button[] buttonList;
     private Button preview;
+
+    private int lampColor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         buttonList = new Button[6];
+        lampColor = getResources().getColor(R.color.red);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -32,13 +36,64 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0; i < main.getChildCount(); i++){
             View child = main.getChildAt(i);
             if (child instanceof Button){
-                buttonList[i] = (Button) child;
+                if(child.getId() == R.id.colorPreview){
+                    preview = (Button) child;
+                    preview.setBackgroundColor(lampColor);
+                } else {
+                    buttonList[i] = (Button) child;
+                    child.setOnClickListener(this);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View button) {
+        int red = Color.red(lampColor);
+        int green = Color.green(lampColor);
+        int blue = Color.blue(lampColor);
+
+        int id = button.getId();
+
+        if (id == R.id.addRed) {
+            red += 10;
+
+            if(red > 255){
+                red = 255;
+            }
+        } else if (id == R.id.remRed) {
+            red -= 10;
+
+            if(red < 0){
+                red = 0;
+            }
+        } else if (id == R.id.addGreen) {
+            green += 10;
+
+            if(green > 255){
+                green = 255;
+            }
+        } else if (id == R.id.remGreen) {
+            green -= 10;
+
+            if(green < 0){
+                green = 0;
+            }
+        } else if (id == R.id.addBlue) {
+            blue += 10;
+
+            if(blue > 255){
+                blue = 255;
+            }
+        } else if (id == R.id.remBlue) {
+            blue -= 10;
+
+            if(blue < 0){
+                blue = 0;
             }
         }
 
-        buttonList[0] = findViewById(R.id.addRed);
-        buttonList[1] = findViewById(R.id.remRed);
-
-
+        lampColor = Color.rgb(red, green, blue);
+        preview.setBackgroundColor(lampColor);
     }
 }
