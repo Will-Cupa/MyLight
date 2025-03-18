8da58d7cf881e450f342baf92c17635dfc51c837
Alain Sandoz <alain.sandoz@universite-paris-saclay.fr>
2025-03-18 17:27:51 +0100

    some new things

--- app/src/main/java/com/example/mylight/ColorPicker.java
+++ app/src/main/java/com/example/mylight/ColorPicker.java
@@ -9,6 +9,7 @@ import android.view.View;
 
 public class ColorPicker extends View {
     private Paint piePaint;
+    private float diameter;
     public ColorPicker(Context context, AttributeSet attrs) {
         super(context, attrs);
         piePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -17,8 +18,22 @@ public class ColorPicker extends View {
     }
 
     @Override
-    public void onDraw(Canvas canvas) {
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        // Account for padding.
+        float xpad = (float)(getPaddingLeft() + getPaddingRight());
+        float ypad = (float)(getPaddingTop() + getPaddingBottom());
+
+        float ww = (float)w - xpad;
+        float hh = (float)h - ypad;
+
+        // Figure out how big you can make the pie.
+        diameter = Math.min(ww, hh);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(0,0,100, piePaint);
+        canvas.drawCircle(0,0,diameter/2, piePaint);
     }
 }
