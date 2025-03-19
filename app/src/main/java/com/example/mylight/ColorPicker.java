f632fa2624ed8c827ce34792c83937a0aa4e08fe
Alain Sandoz <alain.sandoz@universite-paris-saclay.fr>
2025-03-19 17:48:33 +0100

    J'vais me suicider

--- app/src/main/java/com/example/mylight/ColorPicker.java
+++ app/src/main/java/com/example/mylight/ColorPicker.java
@@ -78,18 +78,18 @@ public class ColorPicker extends View {
         c.drawCircle(width/2, height/2, diameter/2, gradPaint);
 
         // Reset Xfermode
-        //gradPaint.setXfermode(null);
+        gradPaint.setXfermode(null);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        // hsv[0] --> hue
-        // hsv[1] --> saturation
-        // hsv[2] --> value
 
         float angle = 0;
         while(angle <360){
+            // hsv[0] --> hue
+            // hsv[1] --> saturation
+            // hsv[2] --> value
             gradientColors[(int)angle] = Color.HSVToColor(new float[]{angle,1,1});
             angle += step;
         }
